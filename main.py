48769893ce608a5076de588401a9e0d3d874c02e
Vladimir Semenov <33404130+subatiq@users.noreply.github.com>
2023-02-11 12:52:52 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 import time
 import datetime
 import subprocess
@@ -5,16 +6,17 @@ from typing import Dict
 
 
 def run(config: Dict[str, str]):
-    print('[timeburglar] Disabling autosync', subprocess.check_output(f'sudo timedatectl set-ntp false', shell=True))
+    shift = int(config.get('shift', 5))
 
-    while True:
-        now = datetime.datetime.now()
-        print(now)
-        shift = datetime.timedelta(seconds=5)
-        shifted = now - shift
-        print(shifted)
+    print(f'Shift time back by {shift}')
+    print('Disabling autosync on host...')
+    subprocess.check_output(f'sudo timedatectl set-ntp false', shell=True)
 
-        print('[timeburglar] Shifting time backwards', subprocess.check_output(f'sudo date -s "{shifted}"', shell=True))
-        time.sleep(20)
+    now = datetime.datetime.now()
+    print('Was:', now)
+    shift = datetime.timedelta(seconds=shift)
+    shifted = now - shift
+    print('Became:', shifted)
 
-    print('[timeburglar] Enabling autosync', subprocess.check_output(f'sudo timedatectl set-ntp true', shell=True))
+    print('Shifting time bacwards', subprocess.check_output(f'sudo date -s "{shifted}"', shell=True))
+    print('Real time from Google:', subprocess.check_output("curl -I 'https://google.com/' 2>/dev/null | grep -i '^date:' | sed 's/^[Dd]ate: //g'", shell=True))
